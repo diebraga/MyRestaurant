e3d9dadeaf279a8ccf22b9b9478a6a7d4597ea62
Diego Braga <diegobraga@hertz.com>
2023-01-18 12:22:15 +0000

    home initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
-    <Flex width="100%" bg="red.100" minHeight="100vh" alignItems="center">
-      <Flex maxWidth="650px" flexDir="column" bg="amber.300" width="100%">
+    <Flex width="100%" minHeight="100vh" alignItems="center">
+      <Flex maxWidth="650px" flexDir="column" width="100%">
         <Home />
       </Flex>
     </Flex>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Screen } from "../../components/Screen/Screen";
-import { Flex } from "native-base";
+import { Button, Column, Flex, HStack, Heading, VStack } from "native-base";
 
 const Home: React.FC = () => {
   return (
-    <Flex width="100%" bg="amber.100">
-      HOME
+    <Flex
+      width="100%"
+      bg="amber.100"
+      justify="center"
+      minHeight="100vh"
+      paddingX={["3", "8"]}
+    >
+      <Column width="100%" alignItems="center" space={4}>
+        <Heading fontWeight="medium" fontSize={["4xl"]} color="primary.500">
+          MENU
+        </Heading>
+        <Button variant="outline" borderRadius="full" width="100%">
+          Beers
+        </Button>
+        <Button variant="outline" borderRadius="full" width="100%">
+          Food
+        </Button>
+      </Column>
     </Flex>
   );
 };
