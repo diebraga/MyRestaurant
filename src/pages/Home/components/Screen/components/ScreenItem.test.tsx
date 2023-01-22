6d0dc27649b71b09a72bf7c82a2bf9a01ec42043
Diego Braga <diegobraga@hertz.com>
2023-01-22 21:55:05 +0000

    added screen click test

--- src/pages/Home/components/Screen/components/ScreenItem.test.tsx
+++ src/pages/Home/components/Screen/components/ScreenItem.test.tsx
@@ -1,12 +1,22 @@
-import { renderWithProvider, screen } from "../../../../../test";
+import { fireEvent, renderWithProvider, screen } from "../../../../../test";
 import { ScreenItem, ScreenItemProps } from "./ScreenItem";
 
+const mockClick = jest.fn();
+
 const mockScreenItemProps = {
-  name: "name_mock"
-} as ScreenItemProps
+  name: "name_mock",
+  onPress: mockClick as (() => void) | undefined,
+} as ScreenItemProps;
 
 it("Should render ScreenItem corectly", () => {
   renderWithProvider(<ScreenItem {...mockScreenItemProps} />);
 
   expect(screen.getByText(mockScreenItemProps.name)).toBeDefined();
+});
+
+it("Should trigger onPress corectly", () => {
+  renderWithProvider(<ScreenItem {...mockScreenItemProps} />);
+
+  fireEvent.click(screen.getByText(mockScreenItemProps.name));
+  expect(mockClick).toBeCalled();
 });
